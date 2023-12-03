7e0278b50a75faaf5d7e887c8796cecf72b75407
mowzlisre <mowzlisre@icloud.com>
2023-12-02 20:50:51 -0500

    Phase 1 commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     print(">>> Automated Data Insertion - Neo4J & MongoDB")
     # Data Loading
     print(">>> Loading the CSV file")
-    with open('tracks_features.csv', newline='') as csvfile:
+    with open('sample.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         data = list(reader)
         print(">>> Loading CSV file succesful")
@@ -19,21 +19,21 @@ if __name__ == "__main__":
     pr_end = time.time()
     print(f">>> Data Preprocessing completed in {pr_end-pr_init-4:.2f}s")
 
-    # Import Data to Neo4J Database
-    print(">>> Initializing the Data Import to Neo4j")
-    nj_init = time.time()
-    neo4jProcessor.importDB(data, albums_data, artists_data, ab_tr_rel, at_ab_rel)
-    nj_end = time.time()
-    print(f">>> {len(data)} Tracks, {len(albums_data)} Albums, {len(artists_data)} Artists nodes and relationships imported succesfully in {nj_end-nj_init:.2f}s")
+    # # Import Data to Neo4J Database
+    # print(">>> Initializing the Data Import to Neo4j")
+    # nj_init = time.time()
+    # neo4jProcessor.importDB(data, albums_data, artists_data, ab_tr_rel, at_ab_rel)
+    # nj_end = time.time()
+    # print(f">>> {len(data)} Tracks, {len(albums_data)} Albums, {len(artists_data)} Artists nodes and relationships imported succesfully in {nj_end-nj_init:.2f}s")
 
-    # # Import Data to MongoDB Database
-    # print(">>> Initializing the Data Import to MongoDB")
-    # mg_init = time.time()
-    # mongoProcessor.importDB(data, albums_data, artists_data)
-    # mg_end = time.time()
-    # print(f">>> {len(data)} Tracks, {len(albums_data)} Albums, {len(artists_data)} Artists documents imported succesfully in {mg_end-mg_init:.2f}s")
-    # end = time.time()
-    # print(f">>> Automated Data Insertion Completed in {end-init-4:.2f}s")
+    # Import Data to MongoDB Database
+    print(">>> Initializing the Data Import to MongoDB")
+    mg_init = time.time()
+    mongoProcessor.importDB(data, albums_data, artists_data)
+    mg_end = time.time()
+    print(f">>> {len(data)} Tracks, {len(albums_data)} Albums, {len(artists_data)} Artists documents imported succesfully in {mg_end-mg_init:.2f}s")
+    end = time.time()
+    print(f">>> Automated Data Insertion Completed in {end-init-4:.2f}s")
 
 
     
